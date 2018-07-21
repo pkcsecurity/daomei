56f98dd81042420c0dd36d897f1b6ed09a6cf6f6
Becker Polverini <becker@pkcsecurity.com>
2018-07-21 07:16:17 -0700

    removed geocoding

--- src/daomei/cljs/map.cljs
+++ src/daomei/cljs/map.cljs
@@ -2,25 +2,31 @@
   (:require [reagent.core :as r]
             [daomei.cljs.controller :as c]))
 
-(def map-obj (atom nil))
+(defn add-pin [map-obj lat-lng]
+  (google.maps.Marker. #js 
+                       {:position lat-lng
+                        :map map-obj}))
 
-(def map-style (atom nil))
+(def center #js {:lat 34.9862055
+                 :lng 102.8387241})
+
 
 (defn google-map []
   (r/create-class
     {:component-did-mount
      (fn [this]
        (let [node (r/dom-node this)]
-         (let [center #js {:lat 34.9862055
-                           :lng 102.8387241}]
-           (c/get-style
-             (fn [style]
-               (reset! map-obj
-                 (google.maps.Map. node
-                   #js {:zoom 4
-                        "gestureHandling" "cooperative"
-                        :center center
-                        :styles (clj->js style)})))))))
+         (c/get-style
+           (fn [style]
+             (let [map-obj (google.maps.Map. node
+                                             #js {:zoom 4
+                                                  "gestureHandling" "cooperative"
+                                                  :center center
+                                                  :styles (clj->js style)})]
+               (c/get-points
+                 (fn [lat-lngs]
+                   (doseq [l-l lat-lngs] 
+                     (add-pin map-obj l-l)))))))))
      :reagent-render
      (fn []
        [:div
@@ -28,32 +34,8 @@
                  :height "400px"
                  :background-color :gray}}])}))
 
-(defn ^:export init-map []
-  )
-
-(defn add-pin [address]
-  (let [gc (google.maps.Geocoder.)]
-    (.geocode gc
-      #js {:address address}
-      (fn [obj]
-        (if-let [lat-lng (aget obj 0 "geometry" "location")]
-          (google.maps.Marker. #js {:position lat-lng
-                                    :map @map-obj})
-          (.error js/console "not found"))))))
-
-(defn address-field []
-  (let [value (r/atom "")]
-    (fn []
-      [:div
-       [:input
-        {:value @value
-         :on-change #(reset! value (.. % -target -value))}]
-       [:button
-        {:on-click #(add-pin @value)}
-        "Add"]])))
-
+(defn ^:export init-map [])
 
 (defn map-body []
   [:div
-    [address-field]
-    [google-map]])
+   [google-map]])
